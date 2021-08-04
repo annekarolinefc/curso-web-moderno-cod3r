ec62d818da24d3d23b2a159f8fdad91683c4ca81
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-04 10:24:52 -0300

    Funções Anonimas

--- JAVASCRIPT/4- Função/funcaoAnonima.js
+++ JAVASCRIPT/4- Função/funcaoAnonima.js
@@ -1,3 +1,15 @@
+//
 (function calculator(a, b, operator) {
     return eval(`${a} ${operator} ${b}`);
 })(50, 2, "/")
+
+const soma = function(x, y) {
+    return x + y
+}
+const imprimirResultado = function(a, b, operacao = soma) {
+    console.log(operacao(a, b))
+}
+
+imprimirResultado(3, 4)
+
+imprimirResultado(3, 4, function(x, y) { return x - y })
