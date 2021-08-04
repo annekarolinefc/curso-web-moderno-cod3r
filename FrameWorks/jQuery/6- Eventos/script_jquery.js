2b4d7d9a72960297f7f7a95823ef662eb8337909
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-04 18:15:23 -0300

    jQuery -> Eventos

---
+++ FrameWorks/jQuery/6- Eventos/script_jquery.js
@@ -0,0 +1,32 @@
+//EVENTOS PARA MOUSE
+//EVENTO CLICK
+$("Button").click(function() {
+    console.log("Botão clicado")
+});
+
+//EVENTO DBLCLICK
+$("Button").dblclick(function() {
+    console.log("Click duplo")
+});
+
+//EVENTO HOVER
+$("Button").hover(function() {
+    console.log("Passou o mouse")
+});
+
+//____________________________
+//EVENTOS PARA NAVEGADOR
+//SCROOL
+//RESIZE
+
+//____________________________
+//EVENTO PARA TECLADO
+//KEYDOWN
+//KEYPRESS
+//KEYUP
+
+//EVENTO PARA FORM 
+//FOCUS
+//BLUR
+//CHANGE
+//SUBMIT
