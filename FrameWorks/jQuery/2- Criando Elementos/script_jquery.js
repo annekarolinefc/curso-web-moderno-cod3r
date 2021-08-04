366d3f01392db3e87c6aef66dad45dadad3ff5e1
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-04 16:47:44 -0300

    jQuery - Criando elementos

---
+++ FrameWorks/jQuery/2- Criando Elementos/script_jquery.js
@@ -0,0 +1,11 @@
+let umadiv = $("#umaDiv");
+let divs = $("div");
+let novoElemento = $("<p>Um novo elemento</p>");
+let outroNovoElemento = $(document.createElement("p"));
+
+//CRIANDO UMA STRING
+let paragrafo = $("<p>Olá Mundo!</p>");
+
+//CRIANDO UM OBJETO
+let elemento = document.createElement("div");
+let elementoJquery = $(elemento);
