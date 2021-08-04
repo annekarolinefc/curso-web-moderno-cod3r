968eadd5b9b6c3b22b513520fcb6563a98d6ff42
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-04 12:32:50 -0300

    jQuery -> esconder botão

---
+++ jQuery/Alterar cor de texto/script_jquery.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#azul').click(function() {
+        $('p').css("color", "blue")
+    });
+
+    $('#vermelho').click(function() {
+        $('p').css("color", "red")
+    });
+})
