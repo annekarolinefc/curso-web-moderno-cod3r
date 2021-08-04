bc265503164d9deed1f3a0073d5f259d971b189f
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-04 12:59:18 -0300

    jQuery -> Encadeamento em bloco

---
+++ FrameWorks/jQuery/Encadeamento em bloco/script_jquery.js
@@ -0,0 +1,24 @@
+//EFEITO FADE
+// Esconde a div de uma maneira bem suave
+$(function() {
+    $('#azul').click(function() {
+        $('p').css("background-color", "blue")
+            .fadeOut() //FAZ DESAPARECER
+            //$('p').hide() -> faz esconder de uma vez
+            .delay(3000) //PAUSA
+            .fadeIn(); //REAPARECER
+    });
+
+    $('#vermelho').click(function() {
+        $('p').css("color", "red")
+        $('#mensagem').text("Cor alterada com sucesso")
+            .css("color", "red")
+            .css("border", "1px solid red")
+            .delay(3000)
+            .fadeOut("fast")
+
+
+    });
+
+
+})
