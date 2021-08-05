73891c6932b179112dbca5d3e2cab1d09b29691a
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-05 19:16:12 -0300

    Tipos de Declaração de Função

---
+++ JAVASCRIPT/4- Função/tipoDeclaracao.js
@@ -0,0 +1,16 @@
+//FUNCTION DECLARATION
+function soma(x, y) {
+    return x + y;
+}
+
+//FUNCTION EXPRESSION
+const sub = function(x, y) {
+    returnx - y;
+}
+console.log(sub(3, 4))
+
+//NAMED FUNCTION EXPRESSION
+const mult = function mult(x, y) {
+    returnx * y;
+}
+console.log(mult(3, 4))
