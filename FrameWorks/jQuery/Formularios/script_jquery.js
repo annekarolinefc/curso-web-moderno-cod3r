c9fbd8e353f234a13a9c6abbf17ec4177ca40acd
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-05 08:57:19 -0300

    Trabalhando com filtros e seletores

---
+++ FrameWorks/jQuery/Formularios/script_jquery.js
@@ -0,0 +1,7 @@
+$(function(){
+    //$(":input").hide(); //ESCONDENDO O FORMULARIO
+    //$('form :text').toggle(); //ESCONDE O ITEM
+    $('form :password').toggle();
+    $('form :radio').toggle();
+    $('form :checkbox').toggle();
+})
