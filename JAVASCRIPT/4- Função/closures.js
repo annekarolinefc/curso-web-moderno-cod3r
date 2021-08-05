5fc0bbb495269819a4fa0ca11007076eff64c35b
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-05 19:16:23 -0300

    Closures

---
+++ JAVASCRIPT/4- Função/closures.js
@@ -0,0 +1,17 @@
+//CLOSURE
+//é oescopo criando quando uma função é declarada
+//Esse escopo permite a função acessar e manipular variáveis externasà função
+
+const x = "Global"
+
+function fora() {
+    const x = "Local"
+
+    function dentro() {
+        return x;
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
