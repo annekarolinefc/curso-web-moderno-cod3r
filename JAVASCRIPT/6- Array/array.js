628aed023337c13e0724ccd3a3278f8da93870c2
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-18 22:21:06 -0300

    Arrays em JS

--- JAVASCRIPT/6- Array/array.js
+++ JAVASCRIPT/6- Array/array.js
@@ -3,3 +3,9 @@
 //Exemplo de Array de strings
 var montadoras = ["vw", "fiat", "ford", "honda"]
 console.log(montadoras[2])
+
+const listaDeCompras = ["arroz", "batata", "maça", "pera"]
+console.log(listaDeCompras[0])
+console.log(listaDeCompras[1])
+console.log(listaDeCompras[2])
+console.log(listaDeCompras[3])
--- JAVASCRIPT/6- Array/forEach.js
+++ JAVASCRIPT/6- Array/forEach.js
@@ -1,6 +1,5 @@
 var montadoras = ["vw", "fiat", "ford", "honda"]
 
-
 //percorrer o array
 montadoras.forEach(function(value, index) {
     console.log(value, index)
