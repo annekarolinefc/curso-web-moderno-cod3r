f17f43a8d81e45abfa16737355e6eb699ea1cc05
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-18 21:58:55 -0300

    Objetos em JS

---
+++ JAVASCRIPT/5- Objeto/objetoPessoa.js
@@ -0,0 +1,20 @@
+const pessoa = {
+    nome: "Anne",
+    idade: 28,
+    endereco: {
+        rua: "Henrique Gorceix",
+        numero: 1670,
+        complemento: "apart 502",
+        bairro: "Monsenhor Messias",
+        cidade: "Belo Horizonte",
+        estado: "Minas Gerais",
+        pais: "Brasil"
+    },
+    ehMaiorDeIdade: true,
+    altura: 1.56
+
+}
+
+console.log(pessoa);
+
+console.log(pessoa.altura);
